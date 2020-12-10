b53f033aa126e1813bdaa5fb44f1b8aae4e8140f
poonampise <65179550+poonampise@users.noreply.github.com>
2020-12-10 17:22:20 +0530

    Add files via upload

---
+++ Day 5/Employee.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5
+{
+    class Employee
+    {
+        public int EmpNo;
+        public string Name;
+        public double Salary;
+        public Employee()
+        {
+
+        }
+
+
+        public void GetEmployee()
+        {
+            Console.WriteLine("enter employee NO ======> ");
+            EmpNo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter employee Name =====>");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter employee Salary =====>");
+            Salary = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void DisplayEmployee()
+        {
+
+            Console.WriteLine(EmpNo + " " + Name + " " + Salary);
+
+        }
+
+
+    }
+
+    class program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter No Of Employee");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            Employee[] E = new Employee[size];
+
+            for (int i = 0; i < size; i++)
+            {
+
+                E[i] = new Employee();
+                E[i].GetEmployee();
+
+            }
+            Console.ReadLine();
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("employee details of Employee No =" + i);
+                E[i].DisplayEmployee();
+            }
+            Console.ReadLine();
+
+            // Console.WriteLine("employee With highest Salary  ===>");
+
+            //Employee maxEmp = E[0];
+            //for(int i = 0; i < size; i++)
+            //{
+
+            //    if (E[i].Salary > maxEmp.Salary)
+            //    {
+            //        maxEmp = E[i];
+            //        maxEmp.DisplayEmployee();
+
+            //    }      
+            //}
+
+            Console.WriteLine("employee with highest salary =>");
+            double temp = 0;
+            for (int i = 0; i < size; i++)
+            {
+
+                if (E[i].Salary > temp)
+                {
+                    temp = E[i].Salary;
+
+
+                }
+
+            }
+
+
+            Console.WriteLine("highest salary = " + temp);
+            Employee details;
+            for (int i = 0; i < size; i++)
+            {
+                details = E[i];
+                if (temp == details.Salary)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("highest salary employee details ===>");
+                    Console.WriteLine("Employee number = " + details.EmpNo);
+                    Console.WriteLine("Employee name = " + details.Name);
+                    Console.WriteLine("Employee Salary = " + details.Salary);
+                }
+            }
+            Console.ReadLine();
+
+            Console.WriteLine("enter emp no tp search emp detail");
+            int empno = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < size; i++)
+            {
+                details = E[i];
+                if (empno == details.EmpNo)
+                {
+                    Console.WriteLine("Employee number = " + details.EmpNo);
+                    Console.WriteLine("Employee name = " + details.Name);
+                    Console.WriteLine("Employee Salary = " + details.Salary);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day 5/Que3.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5
+{
+    class Que3
+    {
+        static void Main()
+        {
+            int i = 0;
+            Student[] s = new Student[5];
+
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine("name of student =");
+                string NAME = Console.ReadLine();
+                Console.WriteLine("Rollno of Student =");
+                int ROLLNO = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Marks of Student =");
+                decimal MARKS = Convert.ToDecimal(Console.ReadLine());
+                s[i] = new Student(NAME, ROLLNO, MARKS);
+            }
+
+
+            Console.ReadLine();
+
+            for (i = 0; i < 5; i++)
+            {
+                s[i].Display();
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public struct Student
+    {
+        private string Name;
+        private int RollNo;
+        private decimal Marks;
+
+        public Student(string NAME, int ROLLNO, decimal MARKS)
+        {
+            this.Name = "noname";
+            this.RollNo = 1;
+            this.Marks = 1;
+            this.NAME = NAME;
+            this.ROLLNO = ROLLNO;
+            this.MARKS = MARKS;
+
+        }
+
+        public string NAME
+        {
+            set
+            {
+                if (value != "")
+                {
+                    Name = value;
+                }
+                else
+                {
+                    Console.WriteLine("invalid name");
+                }
+            }
+            get
+            {
+                return Name;
+            }
+        }
+
+        public int ROLLNO
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    RollNo = value;
+                }
+                else
+                {
+                    Console.WriteLine("wrong rollno");
+                }
+            }
+            get
+            {
+                return RollNo;
+            }
+        }
+
+        public decimal MARKS
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    Marks = value;
+                }
+                else
+                {
+                    Console.WriteLine("invalid marks");
+                }
+            }
+            get
+            {
+                return Marks;
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(Name + " " + RollNo + " " + Marks);
+        }
+    }
+}
