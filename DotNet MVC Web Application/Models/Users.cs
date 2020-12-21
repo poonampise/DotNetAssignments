a0b15af4ac5a081362a64b423bef851f0c0d9ff0
poonampise <65179550+poonampise@users.noreply.github.com>
2020-12-22 00:54:30 +0530

    Add files via upload

---
+++ DotNet MVC Web Application/Models/Users.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace WebApplication2.Models
+{
+    public class Users
+    {
+
+        [Required]
+        public string UserName { get; set; }
+
+        
+        [Required(ErrorMessage = "Please Enter Email Address")]
+        [Display(Name = "UserName")]
+        [RegularExpression(".+@.+\\..+", ErrorMessage = "Please Enter Correct Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Password")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public int CityId { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Mobile No")]
+        [Display(Name = "Mobile")]
+        [StringLength(10, ErrorMessage = "The Mobile must contains 10 characters", MinimumLength = 10)]
+        public string Phone { get; set; }
+        
+
+        public bool IsActive { get; set; }
+        public IEnumerable<SelectListItem> cityList { set; get; }
+    }
+}
