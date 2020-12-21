0dc9082e6ee904c904c1874ed7dbcae1f238a281
poonampise <65179550+poonampise@users.noreply.github.com>
2020-12-21 08:12:16 +0530

    Add files via upload

---
+++ Models/Users.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace WebApplication2.Models
+{
+    public class Users
+    {
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public int CityId { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+        
+
+        public bool IsActive { get; set; }
+        public IEnumerable<SelectListItem> cityList { set; get; }
+    }
+}
